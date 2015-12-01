d95476987e9295044919954d0992723636246680
ktvexe <ktvexe@gmail.com>
2015-12-01 08:44:29 +0800

    it's done

--- mprocess.c
+++ mprocess.c
@@ -15,12 +15,14 @@ int main(int argc,char *argv[]){
     struct shmid_ds buf ;
     int flag = 0 ;
     int pid ,i;
-	int total;	
+	long total = 0;	
 	FILE* fptr;
-	long size,share_size,remain,pnum;
+	long size,share_size,remain,pnum,pos;
 	int *pids;
-	char *readbuf,*readbuf_end,*newbuf;
+	char *readbuf,*addbuf,*readbuf_end,*newbuf;
 	char* search;
+	long len=strlen(argv[2]);
+	printf("len:%ld",len);
 	//check input
 	if(argc !=4){
 		perror("Input error!!");
@@ -52,12 +54,13 @@ int main(int argc,char *argv[]){
     //create child
 	pids = (int*)malloc(pnum*sizeof(int));
 	readbuf = (char*)malloc(share_size*sizeof(char));
+	addbuf = (char*)malloc(len*sizeof(char));
 	readbuf_end = (char*)malloc(remain*sizeof(char));
 	fseek(fptr,0,SEEK_SET);
 	for( i = 0;i<pnum;i++){
 		//child process work
 		if(!( pids[i]= vfork())){
-			printf("child:%d\n",i);
+	//		printf("child:%d\n",i);
 			int count=0;
 			//shared memory
 			shmaddr = (char *)shmat( shmid, NULL, 0 ) ;
@@ -71,7 +74,7 @@ int main(int argc,char *argv[]){
 				fread(readbuf,sizeof(char),share_size,fptr);
 				fread(readbuf_end,sizeof(char),remain,fptr);
 //				printf("readbuf:%s",readbuf);
-//				printf("%s\n",readbuf_end);
+				printf("%s\n",readbuf_end);
 				newbuf = (char*)malloc((share_size+remain)*sizeof(char));
 				strcpy(newbuf,readbuf);
 				strcat(newbuf,readbuf_end);
@@ -92,12 +95,32 @@ int main(int argc,char *argv[]){
 			}
 			//normal process
 			else{
-				fread(readbuf,sizeof(char),share_size,fptr);
+//				printf("fptr-o:%p\n",fptr);
+/*				if(i!=0){
+					fread(readbuf,sizeof(char),share_size,fptr-len);
+				}
+				else
+*/				fread(readbuf,sizeof(char),share_size,fptr);
+//				printf("fptr-o1:%p\n",fptr);
+				pos = ftell(fptr);
+				fread(addbuf,sizeof(char),len,fptr);
+//				printf("position:%ld",ftell(fptr));
 //				printf("readbuf:%s\n",readbuf);
+//				printf("readbuf:%s\n",addbuf);
+				fseek(fptr,pos,SEEK_SET);
+//				printf("fptr-o2:%p\n",fptr);
+				newbuf = (char*)malloc((share_size+len)*sizeof(char));
+				strcpy(newbuf,readbuf);
+				strcat(newbuf,addbuf);
+//				printf("a\n");
+				//fptr -=len;
+//				printf("fptr-f:%p\n",fptr);
+//				printf("b\n");
+				
 				while(1){
-					search=strstr(readbuf,argv[2]);
-					if(strstr(readbuf,argv[2])){
-						readbuf=search+strlen(argv[2]);
+					search=strstr(newbuf,argv[2]);
+					if(strstr(newbuf,argv[2])){
+						newbuf=search+strlen(argv[2]);
 						count++;
 					}
 					else
@@ -124,10 +147,10 @@ int main(int argc,char *argv[]){
 		total += strtol(part,NULL,10);	
 		part =strtok(NULL," ");
 	}
-	printf("total:%d",total);
+	printf("total:%ld\n",total);
 	shmdt(shmaddr);
 	shmctl(shmid,IPC_RMID,NULL);
-
+	fclose(fptr);
     return 0 ;
 
 }
